13d2f379b34e95c629997d4e98f26437c8e013cc
Abhishek Jaiswal <jaiswalabhishek359@gmail.com>
2020-08-07 10:22:41 +0200

    The working File

--- Scripts/ProdToStage.ps1
+++ Scripts/ProdToStage.ps1
@@ -11,7 +11,7 @@ $a = Get-AzWebApp -Name $WebAppName
 $plan = $a.ServerFarmId.Split('/')[-1]
 
 # remove old staging slot
-Remove-AzureRMWebAppSlot -ResourceGroupName $a.ResourceGroup -Name $a.Name -Slot $StagingSlotName -Force
+Remove-AzWebAppSlot -ResourceGroupName $a.ResourceGroup -Name $a.Name -Slot $StagingSlotName -Force
 
 # create new staging slot by copying production
-New-AzureRMWebAppSlot -ResourceGroupName $a.ResourceGroup -Name $a.Name -AppServicePlan $plan -Slot $StagingSlotName -SourceWebApp $a
+New-AzWebAppSlot -ResourceGroupName $a.ResourceGroup -Name $a.Name -AppServicePlan $plan -Slot $StagingSlotName -SourceWebApp $a
